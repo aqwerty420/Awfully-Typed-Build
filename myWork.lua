95b352156b8ac77a46faa9af14b45432c37b6c48
aqwerty420 <aqwerty420@protonmail.com>
2023-06-17 12:21:10 +0000

    Update from https://github.com/aqwerty420/Awfully-Typed/commit/c03ff0921fc46887f93ab9a24fe3771e67f0ee1b

--- myWork.lua
+++ myWork.lua
@@ -1,9 +1,88 @@
 local Unlocker, awful, myWork = ...
-local ____lualib = myWork["lualib_bundle"]
+local ____modules = {}
+local ____moduleCache = {}
+local ____originalRequire = require
+local function require(file, ...)
+    if ____moduleCache[file] then
+        return ____moduleCache[file].value
+    end
+    if ____modules[file] then
+        local module = ____modules[file]
+        ____moduleCache[file] = { value = (select("#", ...) > 0) and module(...) or module(file) }
+        return ____moduleCache[file].value
+    else
+        if ____originalRequire then
+            return ____originalRequire(file)
+        else
+            error("module '" .. file .. "' not found")
+        end
+    end
+end
+____modules = {
+["lualib_bundle"] = function(...) 
+local function __TS__Class(self)
+    local c = {prototype = {}}
+    c.prototype.__index = c.prototype
+    c.prototype.constructor = c
+    return c
+end
+
+local function __TS__New(target, ...)
+    local instance = setmetatable({}, target.prototype)
+    instance:____constructor(...)
+    return instance
+end
+
+return {
+  __TS__Class = __TS__Class,
+  __TS__New = __TS__New
+}
+ end,
+["gui"] = function(...) 
+local green = {170, 211, 114, 1}
+local white = {255, 255, 255, 1}
+local dark = {21, 21, 21, 0.45}
+local awfulGui, awfulSettings, awfulCmd = awful.UI:New("myWork", {title = "My Work", show = true, colors = {title = green, primary = white, accent = green, background = dark}})
+ end,
+["spells"] = function(...) 
+local ____exports = {}
+local NewSpell = awful.NewSpell
+____exports.Recklessness = NewSpell(1719, {targeted = false})
+____exports.Recklessness:Callback(function(spell)
+    spell:Cast()
+end)
+____exports.Rampage = NewSpell(184367)
+____exports.Rampage:Callback(function(spell)
+    spell:Cast(awful.target)
+end)
+____exports.Execute = NewSpell(5308)
+____exports.Execute:Callback(function(spell)
+    if awful.target.hp <= 20 then
+        spell:Cast(awful.target)
+    end
+end)
+____exports.RagingBlow = NewSpell(85288)
+____exports.RagingBlow:Callback(function(spell)
+    spell:Cast(awful.target)
+end)
+____exports.Bloodthirst = NewSpell(23881)
+____exports.Bloodthirst:Callback(function(spell)
+    spell:Cast(awful.target)
+end)
+____exports.Whirlwind = NewSpell(190411, {targeted = false})
+____exports.Whirlwind:Callback(function(spell)
+    if awful.target.distance <= 8 then
+        spell:Cast()
+    end
+end)
+return ____exports
+ end,
+["myWork"] = function(...) 
+local ____lualib = require("lualib_bundle")
 local __TS__Class = ____lualib.__TS__Class
 local __TS__New = ____lualib.__TS__New
 local ____exports = {}
-local spells = myWork["spells"]
+local spells = require("spells")
 local Dummy = __TS__Class()
 Dummy.name = "Dummy"
 function Dummy.prototype.____constructor(self)
@@ -24,10 +103,14 @@ myWork.warrior.fury:Init(function()
     spells.Bloodthirst()
     spells.Whirlwind()
 end)
-awful.Populate(
-    {
-        ["myWork"] = ____exports,
-    },
-    myWork,
-    getfenv(1)
-)
+____exports.test = function()
+    local a = 1
+end
+local function test2()
+    local a = 1
+end
+____exports.func = {test = ____exports.test}
+return ____exports
+ end,
+}
+require("myWork", ...)
